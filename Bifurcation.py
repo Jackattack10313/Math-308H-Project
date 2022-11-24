46936c6fa727499e415b0eb940797f1917fefe4e
jackethridge1031 <jethridge1031@gmail.com>
2022-11-23 20:18:08 -0600

    Added Bifurcation.py

---
+++ Bifurcation.py
@@ -0,0 +1,41 @@
+import numpy as np
+from scipy.integrate import odeint
+import plotly.express as px
+import pandas as pd
+
+sigma = 10
+b = 8/3
+r_min = 0
+r_max = 100
+r_step = .01
+t_min = 0
+t_max = 120
+t_step = .01
+counter = 0
+
+values = np.zeros([5000000, 2])
+r_vals = np.arange(r_min, r_max, r_step)
+state0 = [1.0, 1.0, 1.0]
+t = np.arange(t_min, t_max, t_step)
+counter = 0
+
+def f(state, t):
+    global counter
+    x, y, z = state  # Unpack the state vector
+    if -1e-2 < x * y - b * z < 1e-2 and counter < values.size / 2:
+        values[counter][0] = r
+        values[counter][1] = z
+        counter += 1
+    return sigma * (y - x), x * (r - z) - y, x * y - b * z  # Derivatives
+
+
+for r in r_vals:
+    if r % 1 == 0:
+        print(r)
+    states = odeint(f, state0, t)
+
+
+df = pd.DataFrame({"r": values[:, 0], "z": values[:, 1]})
+fig = px.scatter(df, x="r", y="z")
+fig.update_traces(marker_size=3)
+fig.show()
--- LorenzAttractor308.py
+++ LorenzAttractor308.py
@@ -3,21 +3,22 @@ from scipy.integrate import odeint
 import plotly.express as px
 import pandas as pd
 
-rho = 28.0
+r = 28.0
 sigma = 10.0
-beta = 8.0 / 3.0
+b = 8.0 / 3.0
 
 
 def f(state, t):
     x, y, z = state  # Unpack the state vector
-    return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  # Derivatives
+    return sigma * (y - x), x * (r - z) - y, x * y - b * z  # Derivatives
 
 
 state0 = [1.0, 1.0, 1.0]
-t = np.arange(0.0, 550.0, 0.0005)
+t = np.arange(0.0, 100.0, 0.0005)
 
 
 states = odeint(f, state0, t)
+print(states)
 
 df = pd.DataFrame({"Convection Rate": states[:, 0], "Horizontal Temperature Variation": states[:, 1], "Vertical Temperature Variation": states[:, 2], "time": t})
 fig = px.scatter_3d(df, x="Convection Rate", y="Horizontal Temperature Variation", z="Vertical Temperature Variation", color="time")
